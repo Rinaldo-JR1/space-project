f4631038279d4470c3c3a7263266d153c4e73ee8
rinaldo-JR1 <rinaldo3uchoa@gmail.com>
2024-02-16 13:16:35 -0300

    Carousel pronto

--- src/app/planets/planets.component.ts
+++ src/app/planets/planets.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { planets } from '../types/planets';
 
 @Component({
   selector: 'app-planets',
@@ -6,9 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './planets.component.scss',
 })
 export class PlanetsComponent {
-  public oi: string[] = ['../../assets/bg.jpg', '../../assets/bg.jpg', '../../assets/bg.jpg'];
-  constructor() { }
-  public teste = (e?:any) => {
-    console.dir(e)
+  public planets: planets[];
+  public text = 'Planets';
+  constructor() {
+    this.planets = [
+      {
+        name: 'Marte',
+        description: 'Marte é o quarto planeta do Sistema Solar, ficando entre a Terra e Júpiter. A 227 milhões de km do Sol, é um planeta formado por superfície rochosa e atmosfera rica em CO2. Representação de Marte, quarto planeta a partir do Sol. Em função de sua coloração, é chamado também de Planeta Vermelho.',
+        image: '../../assets/mars.png',
+      },
+      {
+        name: 'Mars',
+        description: 'Mars2',
+        image: '../../assets/mars.png',
+      },
+    ];
+    this.text = this.planets[0].description;
   }
+  public handlePages = (e?: any) => {
+    this.text = this.planets[e.page].description;
+  };
 }
---
+++ src/app/types/planets.ts
@@ -0,0 +1,5 @@
+export type planets = {
+  name: string;
+  description: string;
+  image: string;
+};
